6e853581a5568d9d930bd34b272f96a02ad767ff
megadubcev2 <aestarodubtsev@edu.hse.ru>
2023-01-24 15:00:30 +0300

    Initial Commit

---
+++ Reasce Line/Reasce_LineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reasce_LineApp.swift
+//  Reasce Line
+//
+//  Created by Andrew on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Reasce_LineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
